9f8e67872b84774e6fa3003a156e4a7920de7100
Kouzen <8497105@qq.com>
2020-07-28 09:14:01 +0800

    加入显示天气的功能

--- app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
@@ -8,5 +8,7 @@ import retrofit2.http.Query
 
 interface PlaceService {
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
-    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+    fun searchPlaces(
+        @Query("query") query: String
+    ): Call<PlaceResponse>
 }
